263cdb4b83294e8705bdf849c1d124fc47c37609
guadamuzmagaly271-netizen <guadamuzmagaly271@gmail.com>
2026-01-28 01:28:20 +0000

    Ordena repositorio

---
+++ Semana  9/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("App de Streamlit")
+st.header("Este es un encabezado")
+st.write("Este es un párrafo")
+st.write("Este es un segundo párrafo")
+st.write("Este es un tercer párrafo")
+
+
